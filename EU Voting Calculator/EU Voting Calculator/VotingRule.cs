96cb10071a867fbedbf4bc581cd680cfc9d5a698
Douglas <dcarrie42@gmail.com>
2020-02-19 19:04:34 +0000

    changed votingrule

--- EU Voting Calculator/EU Voting Calculator/VotingRule.cs
+++ EU Voting Calculator/EU Voting Calculator/VotingRule.cs
@@ -8,14 +8,44 @@ namespace EU_Voting_Calculator
 {
     public class VotingCount
     {
+        int vote;
+        int yes = 0;
+        int no = 0;
+        int abstain = 0;
         Loader load = new Loader();
+
+
+        public void PassRules(string RuleType)
+        {
+
+
+            if (RuleType == "Qualified")
+            {
+                string info = "Minimum “Yes” required for adoption: (55%)";
+
+            }
+            if (RuleType == "Reinforced")
+            {
+                string info = "Minimum “Yes” required for adoption: (72%)";
+            }
+            if (RuleType == "Simple")
+            {
+                string info = "Minimum “Yes” required for adoption: (50%)";
+            }
+            if (RuleType == "Unanimity")
+            {
+                string info = "Minimum “Yes” required for adoption: (100%)";
+
+            }
+
+        }
 
         public void Vote()
         {
-            int vote;
-            int yes = 0;
-            int no = 0;
-            int abstain = 0;
+            vote = 0;
+            yes = 0;
+            no = 0;
+            abstain = 0;
 
             for (int i=0; i< load.countryList.Count; i++)
             {
