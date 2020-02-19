26b9292407b1745c7c25ba8f9c9de58ff54112b5
Douglas <dcarrie42@gmail.com>
2020-02-19 18:11:55 +0000

    new commit

---
+++ EU Voting Calculator/EU Voting Calculator/VotingRule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EU_Voting_Calculator
+{
+    public class VotingRule
+    {
+        Loader load = new Loader();
+
+        public void Vote()
+        {
+            int vote;
+
+            for (int i=0; i< load.countryList.Count; i++)
+            {
+                vote = load.countryList[i].getVote();
+
+                if (vote == 0)
+                {
+
+                }
+                if (vote == 1)
+                {
+
+                }
+                if (vote == 2)
+                {
+
+                }
+            }
+        }
+}
+}
+
