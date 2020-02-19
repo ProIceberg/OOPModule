f516abedb69ce3b96fa67ad9e668a4e1d9bfc75c
Tom <tomh6406@gmail.com>
2020-02-19 18:33:05 +0000

    test

---
+++ EU Voting Calculator/EU Voting Calculator/vScrollBar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EU_Voting_Calculator
+{
+    internal class vScrollBar
+    {
+        internal DockStyle Dock;
+        internal int Width;
+        internal int Height;
+        internal string Name;
+
+        public static implicit operator vScrollBar(VScrollBar v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
